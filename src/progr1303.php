4de9bb8b3a157788d1bc294f05996538281b4673
Ismael Lugo <ilugo.cai@ibm.com>
2023-11-20 20:53:33 -0300

    linea base

---
+++ src/progr1303.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Control 1 - IACC - S1_CONTROL_SET_C_PROGR1303</title>
+</head>
+<body>
+
+<?php
+    echo "<h1>Hola profesor Juan Martinez</h1>";
+?>
+
+</body>
+</html>
